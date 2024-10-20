aeff8f0ed64eed5f21692ce803de9801dac37f3d
jaehyeok.choi <franics_bacon@naver.com>
2024-10-20 18:49:10 +0900

    fix: 적재컬럼 문제 해결

--- load_crypto_marketcap.py
+++ load_crypto_marketcap.py
@@ -11,5 +11,12 @@ conn = BigQueryConn()
 client = CoinMarketCapClient()
 data = client.listing_latest()
 data['reg_date'] = reg_date
+# 문자열을 datetime으로 변환 & UTC에서 KST로 변환
+data['last_updated'] = pd.to_datetime(data['last_updated'], utc=True).dt.tz_convert('Asia/Seoul')
+
+for _c in ['tags', 'platform', 'quote']:
+    data[_c] = data[_c].astype(str)
+
+data["total_supply"] = pd.to_numeric(data['total_supply'])
 
 conn.upsert(data, table_id='coin_marketcap', data_set='crypto_fluxor', target_dict={'reg_date': reg_date})
