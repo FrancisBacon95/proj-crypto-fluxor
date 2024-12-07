8a31a9a2889b8621bfefe52d41b070a791062731
jaehyeok.choi <franics_bacon@naver.com>
2024-12-07 16:43:41 +0900

    fix: fastAPI 기반으로 변경

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import warnings
 # 모든 경고 무시
-warnings.filterwarnings("ignore")
-
 import pandas as pd
 import pytz
 from datetime import datetime, timedelta
 from src.ctrend_model import CTRENDAllocator
-kst = pytz.timezone('Asia/Seoul')
-today = datetime.combine(datetime.now(tz=kst).date(), datetime.min.time())
+from fastapi import FastAPI
+warnings.filterwarnings("ignore")
 
-args = {
-    'train_size': 365*2,
-    'inference_date': today - timedelta(days=1),
-    'except_cryptos': ('KRW-BTC'),
-}
-obj = CTRENDAllocator(**args)
+app = FastAPI()
+# health check
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-raw, outliers = obj.preprocess()
-long, short = obj.run(raw=raw, outliers_for_train=outliers)
-obj.execute_trade_logic(cand_long=long, cand_short=short)
+@app.get('/run')
+def run():
+    kst = pytz.timezone('Asia/Seoul')
+    today = datetime.combine(datetime.now(tz=kst).date(), datetime.min.time())
+
+    args = {
+        'train_size': 365*2,
+        'inference_date': today - timedelta(days=1),
+        'except_cryptos': ('KRW-BTC'),
+    }
+    obj = CTRENDAllocator(**args)
+
+    raw, outliers = obj.preprocess()
+    long, short = obj.run(raw=raw, outliers_for_train=outliers)
+    # obj.execute_trade_logic(cand_long=long, cand_short=short)
+    return {'long': long, 'short': short}
