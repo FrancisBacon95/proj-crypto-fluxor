a6281c00e0c583db67d2074849797f341f07f4fd
jaehyeok.choi <franics_bacon@naver.com>
2024-12-07 19:17:09 +0900

    fix: 거래할 수 있도록 주석 제거

--- src/ctrend_model.py
+++ src/ctrend_model.py
@@ -165,7 +165,7 @@ class CTRENDAllocator():
             if _market in self.except_cryptos:
                 continue
             logger.info(f'SELL(ask) market(시장가) - {_market}:{ _balance}')
-            # self.bithumb.exceute_order(type='sell', market=_market, volume=_balance, ord_type='market')
+            self.bithumb.exceute_order(type='sell', market=_market, volume=_balance, ord_type='market')
         logger.info('WAIT 10sec. FOR SELLING SETTLEMENT')
         time.sleep(10)
 
@@ -182,6 +182,6 @@ class CTRENDAllocator():
             if _market in self.except_cryptos:
                 continue
             logger.info(f'BUY(bid) price(시장가) - {_market}:{each_budget}')
-            # self.bithumb.exceute_order(type='buy', market=_market, price=each_budget, ord_type='price')
+            self.bithumb.exceute_order(type='buy', market=_market, price=each_budget, ord_type='price')
 
         return 1
