864af2aa59a6afc11a04b3bde4a018b5f57ac559
jaehyeok.choi <franics_bacon@naver.com>
2024-12-07 18:55:02 +0900

    fix: print로 바꿔서 확인

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def read_root():
 def run():
     import logging
     logger = logging.getLogger(__name__)
-    logger.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: run()")
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: run()")
     kst = pytz.timezone('Asia/Seoul')
     today = datetime.combine(datetime.now(tz=kst).date(), datetime.min.time())
 
@@ -27,9 +27,9 @@ def run():
         'except_cryptos': ('KRW-BTC'),
     }
     obj = CTRENDAllocator(**args)
-    logger.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: obj")
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: obj")
     raw, outliers = obj.preprocess()
-    logger.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: raw, outliers")
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: raw, outliers")
     long, short = obj.run(raw=raw, outliers_for_train=outliers)
     obj.execute_trade_logic(cand_long=long, cand_short=short)
     return {'result': 'test'}
